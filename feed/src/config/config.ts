4cb4ba619c56bf370bab953ecbb958f7b064abd2
ss-codie <39163189+ss-codie@users.noreply.github.com>
2020-05-10 18:23:12 -0400

    Updated deployment

--- feed/src/config/config.ts
+++ feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': 'postgres',
-  'password': 'Microudac323',
-  'database': 'postgres',
-  'host': 'microservices-udacity.csevjaxdtzvp.us-east-2.rds.amazonaws.com',
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
-  'aws_region': 'us-east-2',
-  'aws_profile': 'default',
-  'aws_media_bucket': 'microservices-561963404157-proj',
-  'url': 'http://localhost:8100',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
   'jwt': {
-    'secret': 'hello',
+    'secret': process.env.JWT_SECRET,
   },
 };
